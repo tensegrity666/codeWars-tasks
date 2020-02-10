7d6e157fb56e84ce2b2f40cd85deb5302f289c06
Roman Yakovlev <tensegrity666@eicloud.com>
2020-02-10 19:31:27 +0300

    feat: complete second task (remove first/end char)

--- solutions.js
+++ solutions.js
@@ -3,3 +3,14 @@ function solution(str) {
 }
 
 console.log(solution('array'));
+
+
+function removeChar(str) {
+  let arr = [];
+  let newArr = [];
+    for (i = 0; i < str.length; ++i) {
+      arr.push(str[i]);
+      newArr = arr.slice(1, str.length-1).join('');
+    }
+  return newArr;
+};
