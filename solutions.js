2253851b939440391abc4aaf552f8790955884bd
Roman Yakovlev <tensegrity666@eicloud.com>
2020-02-14 01:00:41 +0300

    feat: complete some tasks

--- solutions.js
+++ solutions.js
@@ -123,6 +123,7 @@ function findDup(arr) {
   return duplicateValue;
 }
 
+
 //*! is prime (not working for big integers)
 
 function isPrime(num) {
@@ -160,9 +161,132 @@ function isPrime(num) {
   return true;
 }
 
-//shortest 
-
+//shortest
 function isPrime(num) {
   for (var i = 2; i < num; i++) if (num % i == 0) return false;
   return num >= 2;
+}
+
+
+// Count characters in string
+
+function count(string) {
+  const newObj = {};
+  for (let char of string) {
+    newObj[char] = newObj[char] + 1 || 1;
+  }
+  return newObj;
+}
+
+//other
+function count(string) {
+  return string.split('').reduce(function (counts, char) {
+    counts[char] = (counts[char] || 0) + 1;
+    return counts;
+  }, {});
+}
+
+//remove duplicates from list
+function distinct(a) {
+  let distincted = new Set(a);
+  return [...distincted];
+}
+
+//shortest
+function distinct(a) {
+  return [...new Set(a)];
+}
+
+//sum of positive elements
+function positiveSum(arr) {
+  let sum = [];
+
+  if ((arr.length === 0) || (arr.length === 1 && arr[0] === 0)) {
+    return arr = 0;
+  }
+
+  for (let i = 0; i < arr.length; ++i) {
+    if (arr[i] > 0) {
+      sum.push(arr[i]);
+    }
+  }
+
+  const pos = (a, i) => {
+    return a + i;
+  }
+
+  return sum.reduce(pos, 0);
+}
+
+//shortest
+function positiveSum(arr) {
+  return arr.reduce((a, b) => a + (b > 0 ? b : 0), 0);
+}
+
+//FizzBuzz
+function fizzbuzz(n) {
+  const arr = [];
+  for (let i = 1; i <= n; i++) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      arr.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      arr.push('Fizz');
+    } else if (i % 5 === 0) {
+      arr.push('Buzz');
+    } else {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
+
+//shortest
+var fizzify = fizzbuzz = function (n) {
+  return Array.apply(null, new Array(n)).map(function (e, i) {
+    return (++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i;
+  });
+}
+
+//find length of shortest word in string
+function findShort(s) {
+  const arr = s.split(' ');
+  arr.sort((a, b) => {
+    if (a.length > b.length) {
+      return 1;
+    }
+    if (a.length < b.length) {
+      return -1;
+    }
+    return 0;
+  });
+  return arr[0].length;
+}
+
+//shortest
+function findShort(s) {
+  return Math.min(...s.split(" ").map(s => s.length));
+}
+
+//square every digit
+function squareDigits(num) {
+  return +num.toString().split('').map(num => num ** 2).join('');
+}
+
+//Easy mathematical callback
+function processArray(arr, callback) {
+  function callback(a) {
+    if (arr[0] === 4) {
+      return a.map(a => a * 2)
+    }
+    if (arr[0] === 7) {
+      return a.map(a => a + 5)
+    }
+    if (arr[0] === -1) {
+      return a.map(a => a ** 3)
+    }
+    else {
+      return [];
+    }
+  }
+  return callback(arr);
 }
